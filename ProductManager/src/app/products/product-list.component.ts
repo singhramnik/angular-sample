1150da453fe993cc1269b6e829cd0d8d7c62241c
Ramnik Singh <singhramnik@yahoo.com>
2019-07-29 19:45:01 +0100

    implemented filtering of the products

--- ProductManager/src/app/products/product-list.component.ts
+++ ProductManager/src/app/products/product-list.component.ts
@@ -13,7 +13,17 @@ export class ProductListComponent
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
-  listFilter: string = 'cart';
+
+  _listFilter: string;
+  get listFilter(): string {
+    return this._listFilter;
+  }
+  set listFilter(value:string){
+    this._listFilter = value;
+    this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter):this.products;
+  }
+
+  filteredProducts: IProduct[];
   products: IProduct[] = [
     {
       "productId": 2,
@@ -46,6 +56,17 @@ export class ProductListComponent
       "imageUrl": "http://runeman.org/clipart/2019/blunderbuss.png"
     },
   ];
+
+  constructor(){
+    this.filteredProducts = this.products;
+    this.listFilter = 'cart';
+  }
+
+  performFilter(filterBy: string): IProduct[]{
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.products.filter((product: IProduct) =>
+          product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1)
+  }
 
   ngOnInit(): void {
     console.log('In OnInit');
