39c9e27c3f34ac397a035aabe8df52f05361882b
Ramnik Singh <singhramnik@yahoo.com>
2019-07-29 21:15:09 +0100

    Added a service and used DI into to the component.

--- ProductManager/src/app/products/product-list.component.ts
+++ ProductManager/src/app/products/product-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { IProduct } from "./product";
-import { CompileShallowModuleMetadata } from "@angular/compiler";
+import { ProductService } from "./product.service";
 
 @Component({
   selector: 'pm-products',
@@ -24,42 +24,9 @@ export class ProductListComponent
   }
 
   filteredProducts: IProduct[];
-  products: IProduct[] = [
-    {
-      "productId": 2,
-      "productName": "Garden Cart",
-      "productCode": "GDN-0023",
-      "releaseDate": "March 18, 2016",
-      "description": "15 gallon capacity rolling garden cart",
-      "price": 32.99,
-      "starRating": 4.2,
-      "imageUrl": "http://runeman.org/clipart/2019/spade.png"
-    },
-    {
-      "productId": 5,
-      "productName": "Hammer",
-      "productCode": "TBX-0048",
-      "releaseDate": "May 21, 2016",
-      "description": "Curved claw steel hammer",
-      "price": 8.9,
-      "starRating": 4.8,
-      "imageUrl": "http://runeman.org/clipart/2018/hammer-chinese.png"
-    },
-    {
-      "productId": 8,
-      "productName": "Saw",
-      "productCode": "TBX-0022",
-      "releaseDate": "May 15, 2016",
-      "description": "15-inch steel blade hand saw",
-      "price": 11.55,
-      "starRating": 3.7,
-      "imageUrl": "http://runeman.org/clipart/2019/blunderbuss.png"
-    },
-  ];
+  products: IProduct[] = [];
 
-  constructor(){
-    this.filteredProducts = this.products;
-    this.listFilter = 'cart';
+  constructor(private productService: ProductService){
   }
 
   onRatingClicked(message: string): void{
@@ -73,7 +40,8 @@ export class ProductListComponent
   }
 
   ngOnInit(): void {
-    console.log('In OnInit');
+    this.products = this.productService.getProducts();
+    this.filteredProducts = this.products;
   }
 
   toggleImage(): void{
