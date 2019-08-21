e1c2ed946ef04b3c6acc4b303dc9e56d63201cb5
Ramnik Singh <singhramnik@yahoo.com>
2019-08-21 16:16:28 +0100

    Add conditional validation

--- ProductManager/src/app/customer/customer.component.ts
+++ ProductManager/src/app/customer/customer.component.ts
@@ -20,6 +20,7 @@ export class CustomerComponent implements OnInit {
       lastName: ['', [Validators.required, Validators.maxLength(50)]],
       email: ['', [Validators.required, Validators.email]],
       phone: '',
+      notification: 'email',
       sendCatalog: true
     });   
   }
@@ -45,5 +46,15 @@ export class CustomerComponent implements OnInit {
   save(){
     console.log(this.customerForm);
     console.log('Saved: ' + JSON.stringify(this.customerForm.value));
+  }
+
+  setNotification(notifyVia: string): void {
+    const phoneControl = this.customerForm.get('phone');
+    if(notifyVia === 'text'){
+      phoneControl.setValidators(Validators.required);
+    } else {
+      phoneControl.clearValidators();
+    }
+    phoneControl.updateValueAndValidity();
   }
 }
