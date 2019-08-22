4c60a58e8d5fdba3cf84ab3b8d1a9772b0572bbe
Ramnik Singh <singhramnik@yahoo.com>
2019-08-22 09:53:09 +0100

    Cross field validation

--- ProductManager/src/app/customer/customer.component.ts
+++ ProductManager/src/app/customer/customer.component.ts
@@ -3,6 +3,23 @@ import { FormGroup, FormBuilder, Validators, AbstractControl, ValidatorFn } from
 
 import { Customer } from './customer';
 
+function emailMatcher(c: AbstractControl): { [key: string]: boolean } | null {
+  const emailControl = c.get('email');
+  const confirmControl = c.get('confirmEmail');
+
+  if(emailControl.pristine || confirmControl.pristine){
+    return null;
+  }
+  console.log(emailControl.value);
+  console.log(confirmControl.value);
+
+  if (emailControl.valid === confirmControl.value) {
+    return null;
+  }
+
+  return { 'match': true };
+}
+
 function ratingRange(min: number, max: number): ValidatorFn {
   return (c: AbstractControl): { [key: string]: boolean } | null => {
     if (c.value !== null && (isNaN(c.value) || c.value < min || c.value > max)) {
@@ -27,7 +44,10 @@ export class CustomerComponent implements OnInit {
     this.customerForm = this.fb.group({
       firstName: ['', [Validators.required, Validators.minLength(3)]],
       lastName: ['', [Validators.required, Validators.maxLength(50)]],
-      email: ['', [Validators.required, Validators.email]],
+      emailGroup: this.fb.group({
+        email: ['', [Validators.required, Validators.email]],
+        confirmEmail: ['', Validators.required],
+      }, {validator: emailMatcher}),
       phone: '',
       notification: 'email',
       rating: [null, ratingRange(1, 5)],
